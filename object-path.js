adaa1c8047be3e02a2c286aba2327a0a50e00f6f
Falaleev Maxim <max@studio107.ru>
2018-01-22 21:46:37 +0300

    init

---
+++ object-path.js
@@ -0,0 +1,56 @@
+let getKey = key => {
+    let intKey = parseInt(key, 10);
+    return intKey.toString() === key ? intKey : key;
+};
+
+let hasShallowProperty = (obj, prop) => {
+    // to handle objects with null prototypes (too edge case?)
+    if (obj === null) {
+        return false;
+    }
+
+    if (typeof prop === 'number' && Array.isArray(obj)) {
+        return true;
+    }
+
+    return Object.prototype.hasOwnProperty.call(obj, prop);
+};
+
+let getShallowProperty = (obj, prop) => {
+    if (hasShallowProperty(obj, prop)) {
+        return obj[prop];
+    }
+};
+
+const getFromObject = (obj, path, defaultValue) => {
+    if (typeof path === 'number') {
+        path = [path];
+    }
+
+    if (!path || path.length === 0 || obj === null) {
+        return defaultValue;
+    }
+
+    if (typeof path === 'string') {
+        if (path in obj) {
+            return getShallowProperty(obj, path);
+        } else {
+            return getFromObject(obj, path.split('.'), defaultValue);
+        }
+    }
+
+    let currentPath = getKey(path[0]),
+        nextObj = getShallowProperty(obj, currentPath);
+
+    if (nextObj === void 0) {
+        return defaultValue;
+    }
+
+    if (path.length === 1) {
+        return nextObj;
+    }
+
+    return getFromObject(obj[currentPath], path.slice(1), defaultValue);
+};
+
+module.exports = getFromObject;
